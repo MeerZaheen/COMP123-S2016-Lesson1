07a0b31548ed5c0e0a4918680ffd86e7e7a77714
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-10 14:24:46 -0400

    Added Successful Test for HelloWorldToConsole

---
+++ HelloWorldTestProject/HelloWorldUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTestProject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void HelloWorldToConsole()
+        {
+            // Arrange
+            string expectedResult = "Hello, World!";
+
+            // Act
+            string result = COMP123_S2016_Lesson1.Program.HelloWorldToConsole();
+
+            // Asert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
